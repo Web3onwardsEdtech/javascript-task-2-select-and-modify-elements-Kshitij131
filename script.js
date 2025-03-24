fe6d56e4da70ea04d47c61c91942ef8c83ccd2b3
Kshitij131 <1ms23cy032@msrit.edu>
2025-03-24 19:34:38 +0530

    Completed task 2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+const text = document.getElementById('text');
+
+
+const button1 = document.getElementById('button1');
+const button2=document.getElementById('button2');
+
+
+button1.addEventListener('click', () => {
+    text.innerText = 'Button 1 clicked!';
+});
+
+button2.addEventListener('click', () => {
+    text.innerText = 'Button 2 clicked!';
+});
